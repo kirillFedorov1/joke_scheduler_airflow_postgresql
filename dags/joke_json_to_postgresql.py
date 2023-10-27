6129d2ed17d72069f907b6c12dd83ead41b1746c
Кирилл Федоров <hxxxrz@gmail.com>
2023-10-27 14:19:52 +0300

    Глобальные правки

--- dags/joke_json_to_postgresql.py
+++ dags/joke_json_to_postgresql.py
@@ -1,82 +1,89 @@
-from datetime import timedelta
 from airflow.decorators import dag, task
-import pendulum
 from airflow.operators.empty import EmptyOperator
-import requests
-import json
-import logging
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
-
-JOKE_API_URL = "https://official-joke-api.appspot.com/jokes/random"
-DB_TABLE = "airflow_studies.jokes"
+from datetime import timedelta
+import pendulum
+import requests
+import logging
+import pandas as pd
 
 default_args = {
     "owner": "kirill_fedorov",
-    "depends_on_past": False,
-    "email_on_failure": False,
-    "email_on_retry": False,
     "retries": 0,
-    "retry_delay": timedelta(minutes=5),
-    "timeout": 300,
+    "execution_timeout": timedelta(seconds=300),
 }
 
 
 @dag(
+    'joke_dag',
+    description="Парсинг шутки https://official-joke-api.appspot.com/jokes/random",
     default_args=default_args,
-    description=f"Парсинг шутки {JOKE_API_URL}",
-    schedule="@hourly",
-    start_date=pendulum.today('UTC').add(days=-1),
+    #schedule="0 * * * *",
+    schedule=None,
     catchup=False,
+    start_date=pendulum.datetime(2023, 10, 25, tz="UTC"),
 )
 def joke_dag():
-    f"""
+    """
     Парсинг шутки из json
-    по адресу {JOKE_API_URL}
-    в таблицу {DB_TABLE} postgresql.
+    по адресу https://official-joke-api.appspot.com/jokes/random
+    в таблицу airflow_studies.jokes postgresql.
     Реализована обработка конфликта по полю joke_id:
     конфликтная запись обновляется.
     """
 
     start = EmptyOperator(task_id="start")
 
+    end = EmptyOperator(task_id="end")
+
     @task
-    def parse_api() -> dict:
+    def parse_api() -> pd.DataFrame:
         try:
-            response = requests.get(JOKE_API_URL, timeout=10)
+            response = requests.get('https://official-joke-api.appspot.com/jokes/random', timeout=10)
             response.raise_for_status()
+
             if "setup" not in response.text:
                 raise ValueError(f"Неожиданный ответ от api: {response.text}")
-            joke_data = json.loads(response.text)
-        except (requests.RequestException, ValueError) as e:
+            
+            df = pd.DataFrame([response.json()])
+        except Exception as e:
             logging.error(e)
             raise
         else:
-            return joke_data
+            return df
 
-    parse_api_py = parse_api()
+    @task
+    def upload_to_stage(df: pd.DataFrame) -> None:
+        hook = PostgresHook(postgres_conn_id='postgres')
 
-    upload_to_db = SQLExecuteQueryOperator(
-        task_id="upload_to_db",
-        conn_id="postgres",
-        sql=f"""
-            INSERT INTO {DB_TABLE} (joke_id, joke_type, setup, punchline)
-            VALUES (%(joke_id)s, %(joke_type)s, %(setup)s, %(punchline)s)
+        df.to_sql(
+            con=hook.get_uri(),
+            schema='airflow_studies',
+            name='stage_jokes',
+            if_exists='replace',
+            index=False,
+            method='multi',
+            chunksize=1000,
+        )
+
+    stage_to_main = SQLExecuteQueryOperator(
+        task_id='stage_to_main',
+        sql='''
+            INSERT INTO airflow_studies.jokes (joke_id, joke_type, setup, punchline)
+            SELECT id, type, setup, punchline FROM airflow_studies.stage_jokes
             ON CONFLICT (joke_id) DO UPDATE SET
-                joke_type = %(joke_type)s,
-                setup = %(setup)s,
-                punchline = %(punchline)s;
-        """,
-        parameters={
-            "joke_id": "{{ ti.xcom_pull(task_ids='parse_api')['id'] }}",
-            "joke_type": "{{ ti.xcom_pull(task_ids='parse_api')['type'] }}",
-            "setup": "{{ ti.xcom_pull(task_ids='parse_api')['setup'] }}",
-            "punchline": "{{ ti.xcom_pull(task_ids='parse_api')['punchline'] }}",
-        },
+                joke_type = EXCLUDED.joke_type,
+                setup = EXCLUDED.setup,
+                punchline = EXCLUDED.punchline;
+            ''',
+        conn_id='postgres',
     )
 
-    end = EmptyOperator(task_id="end")
+    parsed_data = parse_api()
 
-    _ = start >> parse_api_py >> upload_to_db >> end
+    parsed_to_stage = upload_to_stage(parsed_data)
 
+    _ = start >> parsed_to_stage >> stage_to_main >> end
 
-dag = joke_dag()
+joke_dag()
